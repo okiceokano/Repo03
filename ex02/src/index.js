97882ef6df88be73d3068045cd39ab702785a00f
okiceokano <57456536+okiceokano@users.noreply.github.com>
2021-09-15 23:16:24 +0200

    addes ex02

---
+++ ex02/src/index.js
@@ -0,0 +1,11 @@
+function main() {
+    //Only change code below this line 
+    //emina
+    /* emina
+    amar */
+    //Only change code above this line 
+}
+
+console.log(main());
+
+module.exports = main;
